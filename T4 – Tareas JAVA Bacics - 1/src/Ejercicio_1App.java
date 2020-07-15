be923c0f96f7459a9737ab5d580d1b922d705286
danielmac03-curso <danielmaciasgracia13@gmail.com>
2020-07-15 16:38:57 +0200

    Subido ejercicio 1 de T4 – Tareas JAVA Bacics

---
+++ T4 – Tareas JAVA Bacics - 1/src/Ejercicio_1App.java
@@ -0,0 +1,18 @@
+
+public class Ejercicio_1App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*- Declara dos variables numéricas (con el valor que desees), muestra por consola la suma,
+		resta, multiplicación, división y módulo (resto de la división).*/
+		int a = 23, b = 7;
+		
+		System.out.println("La suma de A y B es: " + (a + b));
+		System.out.println("La resta de A y B es: " + (a - b));
+		System.out.println("La multiplicación de A y B es: " + (a * b));
+		System.out.println("La división de A y B es: " + (a / b));
+		System.out.println("La módulo de A y B es: " + (a % b));
+	}
+
+}
